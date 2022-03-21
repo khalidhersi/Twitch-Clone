7efbba081a81fbc749510ef52ff022273241e800
khalidhersi <cipherpolfoundation@gmail.com>
2022-03-21 17:19:06 +0000

    sidenav & channel comp complete

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import "./Channel.scss";
 
 const Channel = (props) => {
-  const {avater, name, followers} = props
+  const {avatar, name, followers} = props
 
 
   return (
     <div className='channel'>
         <div className="channel__details">
-            <img src={avater} alt="" />
+            <img src={avatar} alt="PROFILE PICTURE" className="channel__details__image"/>
             <p className="channel__details__name">{name}</p>
         </div>
-        <p className="channel__followers">{followers}</p>
+        <p className="channel__followers">🔴 {followers}</p>
     </div>
   )
 }
--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import "./SideNav.scss";
-import twitchLogo from "../../assets/images/twitch-logo.png"
 import Channel from '../Channel/Channel';
 
 const SideNav = () => {
+
   return (
     <div className='sidenav'>
-        <h3>FOLLOWED CHANNELS</h3>
-        <Channel avatar={twitchLogo} name="KLD" followers="9001"/>
-        <Channel avatar={twitchLogo} name="Golden-Flame" followers="55"/>
-        <h3>RECOMMENDED CHANNELS</h3>
-        <p className="sidenav__top__show-more">Show More</p>
+      <div className="sidenav__top">
+          <h5>FOLLOWED CHANNELS</h5>
+          <Channel avatar="https://talenthouse-res.cloudinary.com/image/upload/c_limit,fl_progressive,h_1280,w_1280/v1509508646/user-873992/profile/f4me7cow9lybxespvxnb.jpg" name="KLD" followers="9001"/>
+          <Channel avatar="https://en.freejpg.com.ar/asset/900/6c/6c69/F100009454.jpg" name="Golden-Flame" followers="55"/>
+          <h5>RECOMMENDED CHANNELS</h5>
+          <p className="sidenav__top__show-more">Show More</p>
+      </div>
+      <div className="sidenav__bottom">
+        <div className="sidenav__bottom__container">
+          <i className="sidenav__bottom__container__icon fa-solid fa-magnifying-glass"></i>
+          <input type="text" placeholder='Search to Add Friends' className="sidenav__bottom__container__input"/>
+        </div>
+      </div>
     </div>
   )
 }
